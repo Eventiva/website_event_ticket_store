352f8b7126df94c452652b9e074099de7b1fe6a7
Jonathan Stevens <jonathan@resnovas.com>
2025-09-25 23:57:59 +0100

    Add import for event_event module in models initialization
    This update includes the addition of the event_event module import in the models' __init__.py file, enhancing the integration of event-related functionalities within the application.

--- models/__init__.py
+++ models/__init__.py
@@ -4,5 +4,6 @@ from . import product_template
 from . import product_product
 from . import sale_order_line
 from . import sale_order
+from . import event_event
 from . import event_event_ticket
 from . import website
